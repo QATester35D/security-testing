0e0c7766ea94c19f55a52cb7e7d736280074df7b
Shawn LoPorto <sloporto@asi-test.com>
2025-01-17 09:57:14 -0700

    finished password generator

--- generate_password.py
+++ generate_password.py
@@ -2,23 +2,40 @@ import random
 import string
 import time
 
+#################################################################################################
+# Following these common password stanards:
+#  - Must begin with a character
+#  - Must contain:
+#    - At least 1 lowercase letter
+#    - At least 1 uppercase letter
+#    - At least 1 number
+#    - At least 1 special character
+#################################################################################################
 def generate_password(length, punctuation):
-    # Define the characters to use in the password
+    # Define the characters to use in the password; Getting punctuation from what is passed in
     all_characters = string.ascii_letters + string.digits + punctuation
     allASCII_letters=string.ascii_letters
+    allASCII_lowercase_letters=string.ascii_lowercase
+    allASCII_uppercase_letters=string.ascii_uppercase
     allDigits=string.digits
-    letterMin=random.sample(allASCII_letters,1)
+    # Add the minimum required characters from list in comments at top of script
+    letterLowerMin=random.sample(allASCII_lowercase_letters,1)
+    letterUpperMin=random.sample(allASCII_uppercase_letters,1)
     digitMin=random.sample(allDigits,1)
     punctuationMin=random.sample(punctuation, 1)
-    result=letterMin + digitMin + punctuationMin
+    result=letterLowerMin + letterUpperMin + digitMin + punctuationMin
     remainingValues=[x for x in all_characters if x not in result]
-    remainingLength=length-3
-    # result.extend(random.sample(remainingValues,remainingLength))
-    # Use the random module to generate the password
-    # password = ''.join(random.choice(all_characters) for i in range(length))
-    password = result.join(random.choice(all_characters) for i in range(length))
-    random.shuffle(password)
-    return password
+    remainingLength=length-4
+    result.extend(random.sample(remainingValues,remainingLength)) #generate the rest of characters randomly
+    #shuffling the result but need to keep the first character as a letter
+    random.shuffle(result)
+    if result[0] not in allASCII_letters:
+        while True:
+            random.shuffle(result)
+            if result[0] in allASCII_letters:
+                break
+    pswd=str("".join(result))
+    return pswd
 
 password_length = int(input("Input the desired length of your password:"))
 typicalPunctuation="!\";#$%&'()*+,-./:;<=>?@[]^_`{|}~"
